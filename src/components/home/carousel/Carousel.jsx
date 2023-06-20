0100dee2e8ce6f8a3b04a54fb332a4296b33c287
Karen Huamán <karmahucents@gmail.com>
2023-06-20 18:33:51 -0500

    add: using css modules with sidebar and header

--- src/components/home/carousel/Carousel.jsx
+++ src/components/home/carousel/Carousel.jsx
@@ -5,13 +5,13 @@ export const Carousel = () => {
 
   const slides = [
     {
-      url: 'https://images4.alphacoders.com/640/640956.jpg'
+      url: 'https://studioghiblimovies.com/wp-content/uploads/2020/03/barcode-scanners-qr-code-2d-code-creative-barcode.jpg'
     },
     {
-      url: 'https://i.pinimg.com/originals/3f/98/0b/3f980b492a81d70da1e20288f01b7927.jpg'
+      url: 'https://i.pinimg.com/originals/79/66/1c/79661ce094727e32d528874bb9683fae.jpg'
     },
     {
-      url: 'https://www.pixelstalk.net/wp-content/uploads/images6/4K-Anime-Wallpaper-Desktop-1.jpg'
+      url: 'https://i.pinimg.com/originals/95/52/4d/95524d488344134014e7afdaf3ab7adc.jpg'
     },
   ];
 
@@ -31,14 +31,14 @@ export const Carousel = () => {
 
   return (
     //max-w-[1400px] py-16
-    <div className=" h-[700px] w-full m-auto py-0 px-0 relative">
+    <div className=" h-[720px] w-full m-auto py-0 px-0 relative">
       <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className="w-full h-full bg-center bg-cover duration-500"></div>
     {/* Left Arrow */}
-    <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+    <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/50 text-white cursor-pointer">
       <FiChevronLeft onClick={prevSlider} size={30} />
     </div>
     {/* Right Arrow */}
-    <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+    <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/50 text-white cursor-pointer">
       <FiChevronRight onClick={nextSlider} size={30} />
     </div>
     </div>
--- src/components/home/layouts/Header.jsx
+++ src/components/home/layouts/Header.jsx
@@ -1,21 +1,22 @@
+// import { FiImage, FiMenu, FiHome, FiFastForward, FiSettings, FiUser, FiLogOut, FiHeart } from "react-icons/fi";
 import "./header.css";
+import { FiMenu, FiSearch, FiUser, FiShoppingCart } from "react-icons/fi";
 
 export const Header = () => {
   // los estilos en linea no se pueden reutilizar
   return (
-    <header>
-      <a className="logo">Logo</a>
-      <ul className="menu">
-        <li>
-          <a>Inicio</a>
-        </li>
-        <li>
-          <a>Otro</a>
-        </li>
-        <li>
-          <a>Busqueda</a>
-        </li>
-      </ul>
-    </header>
+    <nav className="flex-div">
+      <div className="nav-left flex-div">
+        <FiMenu className="menu-icon"/>
+      </div>
+      <div className="nav-middle">
+        <h1>CITYNEMA</h1>
+      </div>
+      <div className="nav-right flex-div">
+        <FiSearch className="icon-right"/>
+        <FiShoppingCart className="icon-right"/>
+        <FiUser className="icon-right"/>
+      </div>
+    </nav>
   );
 };
