65ce2b9fddf7f7aaab334ced885d8a172949aa11
Karen Huamán <karmahucents@gmail.com>
2023-06-22 00:51:35 -0500

    feature: change sidebar and using toggle

--- src/components/home/layouts/Header.jsx
+++ src/components/home/layouts/Header.jsx
@@ -1,13 +1,22 @@
 // import { FiImage, FiMenu, FiHome, FiFastForward, FiSettings, FiUser, FiLogOut, FiHeart } from "react-icons/fi";
+import { useState } from "react";
 import "./header.css";
-import { FiMenu, FiSearch, FiUser, FiShoppingCart } from "react-icons/fi";
+import { FiMenu, FiSearch, FiUser, FiShoppingCart,
+  FiHeart, FiHome, FiLogOut, FiSettings, FiWatch, FiX } from "react-icons/fi";
 
 export const Header = () => {
+  // intento de toggle para el sidebar
+  const [show, setShow] = useState(true)
   // los estilos en linea no se pueden reutilizar
   return (
+    <>
+    {/* Header */}
     <nav className="flex-div">
       <div className="nav-left flex-div">
-        <FiMenu className="menu-icon"/>
+        <button style={{ backgroundColor: 'pink'}}
+        onClick={() => setShow(true)}>
+          <FiMenu className="menu-icon"/>
+        </button>
       </div>
       <div className="nav-middle">
         <h1>CITYNEMA</h1>
@@ -18,5 +27,27 @@ export const Header = () => {
         <FiUser className="icon-right"/>
       </div>
     </nav>
+    
+    {/* Sidebar */}
+    <div>
+    { show?
+      <section className="sidebar">
+      <button style={{ backgroundColor:'red'}}
+      onClick={() => setShow(false)}>
+        <FiX/>
+      </button>
+    <div className="short-links">
+      <a> <FiHome className="icon"/> Home</a>
+      <a> <FiWatch className="icon" /> Movies</a>
+      <a> <FiHeart className="icon" /> Favorites</a>
+      <a> <FiUser className="icon" /> User</a>
+      <a> <FiSettings className="icon"/> Settings</a>
+      <hr></hr>
+      <a> <FiLogOut className="icon" /> Log Out</a>
+    </div>
+  </section> : null
+    }
+    </div>
+    </>
   );
 };
--- src/components/home/layouts/Layout.jsx
+++ src/components/home/layouts/Layout.jsx
@@ -1,13 +1,13 @@
 import { Header } from "./Header";
 import { Footer } from "./Footer";
-import { Sidebar } from "./Sidebar";
+/* import { Sidebar } from "./Sidebar"; */
 
 // eslint-disable-next-line react/prop-types
 export const Layout = ({ children }) => {
   return (
     <div>
       <Header />
-      <Sidebar />
+      {/* <Sidebar /> */}
       <main>{children}</main>
       <Footer />
     </div>
